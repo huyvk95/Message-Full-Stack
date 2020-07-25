fa107520ff0c5a96857a549331d58194530e002a
Huy <huyvk3110@gmail.com>
2020-07-26 06:16:07 +0700

    Add git ignore config

--- server/dist/router/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-/* INSTANCE */
-let router = express_1.Router();
-/* HOME */
-router.get('/', function (req, res) { res.send('OK'); });
-exports.default = router;
-//# sourceMappingURL=AuthRouter.js.map
