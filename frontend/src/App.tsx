123fa86785ec9e3d85e8cc9df3b6f8d44dbf9e93
Van Huy <huyvk3110@gmail.com>
2020-07-30 09:38:57 +0700

    React i18n added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 import { initialize } from "./action/AppActions";
-import { IStoreState, IAppData } from "./interface/DataInterface";
 import AuthRouter from "./router/AuthRouter";
 import * as api from "./Api";
 
@@ -25,7 +23,7 @@ class App extends Component<IProps> {
     /* Render */
     render() {
         return (
-            <Router><AuthRouter /></Router>
+            <AuthRouter />
         )
     }
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import store from "./Store";
+import "./i18n";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
-const Context = ()=>(
-    <Provider store={store}>
-        <App/>
-    </Provider>
+const Context = () => (
+    <Suspense fallback="loading">
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </Suspense>
 )
 
 ReactDom.render(<Context />, document.getElementById('root'))
