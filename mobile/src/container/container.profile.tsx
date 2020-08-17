7f9a2918ec711635159a8d64739e6ee95466d8a5
Van Huy <huyvk3110@gmail.com>
2020-08-17 22:48:19 +0700

    Add change password dialog

--- mobile/src/container/container.profile.tsx
+++ mobile/src/container/container.profile.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, NativeSyntheticEvent, TextInputFocusEventData, Alert } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { IUserProfileContainer } from "../interface/interface.component";
-import { TextInput, TouchableWithoutFeedback, Switch } from "react-native-gesture-handler";
-import { IFriendData, IStoreState } from "../interface/interface.data";
+import { TouchableWithoutFeedback, Switch } from "react-native-gesture-handler";
+import { IStoreState } from "../interface/interface.data";
 import { connect } from "react-redux";
 import style from "../style";
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -10,12 +10,14 @@ import IoniIcon from "react-native-vector-icons/Ionicons";
 import SubviewHeader from "../component/component.subview.header";
 import AvatarComponent from "../component/component.avatar";
 import baseStyle from "../style/base";
-import { logout } from "../action/action.user";
+import { logout, updateUserData } from "../action/action.user";
 import { toggleNotification, toggleSound } from "../action/action.app";
+import Dialog from "react-native-dialog";
 import socket from "../socket";
 import common from "../common";
 
 const UserProfileContainer = ({ navigation, user, app, logout, toggleSound, toggleNotification }: IUserProfileContainer) => {
+    let [onChangePassword, setOnChangePassword] = useState(false);
     let { _id, email, avatar, firstName, lastName } = user;
     let cstyle = style.userinfo.content;
 
@@ -28,7 +30,7 @@ const UserProfileContainer = ({ navigation, user, app, logout, toggleSound, togg
     }
 
     function onClickChangePassword() {
-        console.log("onClickChangePassword");
+        setOnChangePassword(true)
     }
 
     function onClickLogout() {
@@ -53,124 +55,159 @@ const UserProfileContainer = ({ navigation, user, app, logout, toggleSound, togg
                 <View style={StyleSheet.flatten([
                     cstyle.controlLayout,
                 ])}>
-                    <TouchableWithoutFeedback
-                        style={cstyle.buttonWrap}
-                    >
-                        <IoniIcon
-                            name="notifications"
-                            style={cstyle.buttonIcon}
-                        />
-                        <View style={StyleSheet.flatten([
-                            cstyle.buttonTextWrap,
-                            cstyle.buttonTextContent
-                        ])}>
-                            <Text style={cstyle.buttonText}>Notification</Text>
-                            <Switch
-                                onValueChange={(check) => { toggleNotification(check) }}
-                                value={app.notification}
-                            />
-                        </View>
-                    </TouchableWithoutFeedback>
+                    <ButtonLine
+                        icon={{ ioni: "notifications" }}
+                        title={{ text: "Notification" }}
+                        switchElem={{ onChange: (check: boolean) => { toggleNotification(check) }, value: app.notification }}
+                    />
 
-                    <TouchableWithoutFeedback
-                        style={cstyle.buttonWrap}
-                    >
-                        <Icon
-                            name="music"
-                            style={cstyle.buttonIcon}
-                        />
-                        <View style={StyleSheet.flatten([
-                            cstyle.buttonTextWrap,
-                            cstyle.buttonTextContent
-                        ])}>
-                            <Text style={cstyle.buttonText}>Sound</Text>
-                            <Switch
-                                onValueChange={(check) => { toggleSound(check) }}
-                                value={app.sound}
-                            />
-                        </View>
-                    </TouchableWithoutFeedback>
+                    <ButtonLine
+                        icon={{ fontawesome: "music" }}
+                        title={{ text: "Sound" }}
+                        switchElem={{ onChange: (check: boolean) => { toggleSound(check) }, value: app.sound }}
+                    />
 
-                    <TouchableWithoutFeedback
-                        style={StyleSheet.flatten([
-                            cstyle.buttonWrap,
-                        ])}
+                    <ButtonLine
+                        icon={{ fontawesome: "user-circle-o" }}
+                        title={{ text: "Change avatar" }}
                         onPress={onClickChangerAvatar}
-                    >
-                        <Icon
-                            name="user-circle-o"
-                            style={StyleSheet.flatten([
-                                cstyle.buttonIcon
-                            ])}
-                        />
-                        <View style={cstyle.buttonTextWrap}>
-                            <Text style={StyleSheet.flatten([
-                                cstyle.buttonText
-                            ])}>Change avatar</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+                    />
 
-                    <TouchableWithoutFeedback
-                        style={StyleSheet.flatten([
-                            cstyle.buttonWrap,
-                        ])}
+                    <ButtonLine
+                        icon={{ fontawesome: "edit" }}
+                        title={{ text: "Edit profile" }}
                         onPress={onClickEditProfile}
-                    >
-                        <Icon
-                            name="edit"
-                            style={StyleSheet.flatten([
-                                cstyle.buttonIcon
-                            ])}
-                        />
-                        <View style={cstyle.buttonTextWrap}>
-                            <Text style={StyleSheet.flatten([
-                                cstyle.buttonText
-                            ])}>Edit profile</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+                    />
 
-                    <TouchableWithoutFeedback
-                        style={StyleSheet.flatten([
-                            cstyle.buttonWrap,
-                        ])}
+                    <ButtonLine
+                        icon={{ fontawesome: "unlock" }}
+                        title={{ text: "Change password" }}
                         onPress={onClickChangePassword}
-                    >
-                        <Icon
-                            name="unlock"
-                            style={StyleSheet.flatten([
-                                cstyle.buttonIcon
-                            ])}
-                        />
-                        <View style={cstyle.buttonTextWrap}>
-                            <Text style={StyleSheet.flatten([
-                                cstyle.buttonText
-                            ])}>Change password</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+                    />
 
-                    <TouchableWithoutFeedback
-                        style={StyleSheet.flatten([
-                            cstyle.buttonWrap,
-                        ])}
+                    <ButtonLine
+                        icon={{ fontawesome: "sign-out", style: baseStyle.color.textDanger }}
+                        title={{ text: "Logout", style: baseStyle.color.textDanger }}
                         onPress={onClickLogout}
-                    >
-                        <Icon
-                            name="sign-out"
-                            style={StyleSheet.flatten([
-                                cstyle.buttonIcon,
-                                baseStyle.color.textDanger
-                            ])}
-                        />
-                        <View style={cstyle.buttonTextWrap}>
-                            <Text style={StyleSheet.flatten([
-                                cstyle.buttonText,
-                                baseStyle.color.textDanger
-                            ])}>Logout</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+                    />
                 </View>
             </View>
+            <ChangePasswordDialog show={onChangePassword} setShow={setOnChangePassword} />
         </View>
+    )
+}
+
+const ChangePasswordDialog = connect(null, { updateUserData })(({ show, setShow, updateUserData }: {
+    show: boolean,
+    setShow: Function,
+    updateUserData: Function
+}) => {
+    let [password, setPassword] = useState("")
+    let [confirmPassword, setConfirmPassword] = useState("")
+    let [oldPassword, setOldPassword] = useState("")
+
+    return (
+        <Dialog.Container visible={show}>
+            <Dialog.Title>Change password</Dialog.Title>
+            <Dialog.Input
+                placeholder="Password"
+                placeholderTextColor={baseStyle.color.textLight.color}
+                secureTextEntry={true}
+                onChangeText={(text: string) => { setPassword(text) }}
+            />
+            <Dialog.Input
+                placeholder="Confirm password"
+                placeholderTextColor={baseStyle.color.textLight.color}
+                secureTextEntry={true}
+                onChangeText={(text: string) => { setConfirmPassword(text) }}
+            />
+            <Dialog.Input
+                placeholder="Old password"
+                placeholderTextColor={baseStyle.color.textLight.color}
+                secureTextEntry={true}
+                onChangeText={(text: string) => { setOldPassword(text) }}
+            />
+            <Dialog.Button
+                label="Cancel"
+                onPress={() => {
+                    setShow(false);
+                }}
+            />
+            <Dialog.Button
+                label="Submit"
+                onPress={() => {
+                    if (password === confirmPassword) {
+                        let sc = socket.getSocket()
+                        if (sc) sc.invoke(common.packet.PROFILE,
+                            {
+                                evt: common.event.PROFILE.PUT,
+                                data: { oldPassword, password }
+                            }).then(data => {
+                                updateUserData(data.data)
+                                setShow(false);
+                            }).catch(err => {
+                                // setError(true)
+                            })
+                    } else {
+                        // setError(true)
+                    }
+                }}
+            />
+        </Dialog.Container>
+    )
+})
+
+const ButtonLine = ({ icon, title, onPress, switchElem }: {
+    icon: { ioni?: string, fontawesome?: string, style?: any }
+    title: { text: string, style?: any }
+    onPress?: Function
+    switchElem?: { value: boolean, onChange: Function }
+}) => {
+    let cstyle = style.userinfo.content;
+    let styleIcon = StyleSheet.flatten([
+        cstyle.buttonIcon,
+        icon.style || {}
+    ])
+    let styleTitle = StyleSheet.flatten([
+        cstyle.buttonText,
+        title.style || {}
+    ])
+
+    return (
+        <TouchableWithoutFeedback
+            style={cstyle.buttonWrap}
+            onPress={() => { if (typeof onPress === "function") onPress() }}
+        >
+            {
+                icon.fontawesome ?
+                    <Icon
+                        name={icon.fontawesome}
+                        style={styleIcon}
+                    /> : icon.ioni ?
+                        <IoniIcon
+                            name={icon.ioni}
+                            style={styleIcon}
+                        /> :
+                        <></>
+            }
+            <View style={StyleSheet.flatten([
+                cstyle.buttonTextWrap,
+                cstyle.buttonTextContent
+            ])}>
+                <Text style={styleTitle}>{title.text}</Text>
+                {
+                    switchElem ?
+                        <Switch
+                            onValueChange={(check) => {
+                                if (typeof switchElem.onChange === "function") switchElem.onChange(check)
+                            }}
+                            value={switchElem.value}
+                        />
+                        :
+                        <></>
+                }
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
