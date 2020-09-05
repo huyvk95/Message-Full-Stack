cde7288615c3599a342de9e4a5fb77a92248129f
Huy <huyvk3110@gmail.com>
2020-09-05 17:57:53 +0700

    Update host

--- frontend/src/common/ConfigCommon.ts
+++ frontend/src/common/ConfigCommon.ts
@@ -1,6 +1,6 @@
 // export const HOST = "localhost"
 // export const HOST = "192.168.31.245"
 // export const HOST = "192.168.1.2"
-export const HOST = "127.0.0.1"
+export const HOST = "5.181.217.57"
 export const PORT = 3000
 export const APP_NAME = "Messenger"
